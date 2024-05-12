669b70501a60ff8ca354e94341395df7a8e9ba33
Paul Hutchings <dev@studiop.page>
2024-05-12 10:12:42 -0400

    Create initial project files

---
+++ LocationPatcher/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+
+namespace LocationPatcher
+{
+    public class Program
+    {
+
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .Run(args);
+        }
+
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+
+
+        }
+    }
+}
